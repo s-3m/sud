d90fa0eada25a31d51c55946846de32c0f2f0c42
Roman <s-3m@yandex.ru>
2025-04-06 22:50:07 +0300

    try case details

--- mossud.py
+++ mossud.py
@@ -49,33 +49,70 @@ def wright_to_file(result):
     # writer.close()
 
 
+async def get_case_data(session: aiohttp.ClientSession, case_link: str):
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Language': 'ru,en;q=0.9',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        # 'Cookie': '_ym_uid=1730396567762587290; _ym_d=1736012922; _ym_isad=2; c-dis=1',
+        'Referer': 'https://mos-sud.ru/search?formType=fullForm&uid=&caseNumber=&participant=&processType=3&codex=12.27&judge=&publishingState=af3545b&documentType=&year=2025&caseDateFrom=&caseDateTo=&caseFinalDateFrom=&caseFinalDateTo=&caseLegalForceDateFrom=&caseLegalForceDateTo=&documentStatus=&documentText=',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-User': '?1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 YaBrowser/25.2.0.0 Safari/537.36',
+        'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132", "YaBrowser";v="25.2", "Yowser";v="2.5"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+    }
+
+    params = {
+        'documentMainArticle': '12.27',
+        'publishingState': 'af3545b',
+        'year': '2025',
+        'formType': 'fullForm',
+    }
+    async with session.get(f"https://mos-sud.ru{case_link}", headers=headers) as response:
+        soup = bs(await response.text(), "lxml")
+        all_rows = soup.find_all("div", class_="row_card")
+        mini_tabs = soup.find("div", id="sessions").find("tbody").find_all("td")
+        print('asass')
+
+
+
+
+
+
+
 async def get_gather_data():
     print("Анализирую данные...")
     tasks = []
     result = []
     async with (aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session):
-        for year in years:
-            for cur_state in cur_states:
-                params = {'year': year, 'formType': 'on', }
-                for item in codex:
-                    params["publishingState"] = cur_state[0]
-                    params["codex"] = item
-                    async with session.get(mos_sud_url, params=params, headers=headers, cookies=cookies) as response:
-                        await asyncio.sleep(2)
-                        soup = bs(await response.text(), 'lxml')
-                        check = soup.find("div", class_="resultsearch_text").text
-                        if check.strip() == "По вашему запросу ничего не найдено":
-                            continue
-                        else:
-                            try:
-                                paginator = soup.find("div", class_="pagenav").find('input').get('value')
-                            except AttributeError:
-                                paginator = 1
+        # for year in years:
+        for cur_state in cur_states[:1]:
+            params = {'year': "2025", 'formType': 'on', }
+            for item in codex[:1]:
+                params["publishingState"] = cur_state[0]
+                params["codex"] = item
+                async with session.get(mos_sud_url, params=params, headers=headers, cookies=cookies) as response:
+                    await asyncio.sleep(2)
+                    soup = bs(await response.text(), 'lxml')
+                    check = soup.find("div", class_="resultsearch_text").text
+                    if check.strip() == "По вашему запросу ничего не найдено":
+                        continue
+                    else:
+                        try:
+                            paginator = soup.find("div", class_="pagenav").find('input').get('value')
+                        except AttributeError:
+                            paginator = 1
 
-                        for page_num in range(1, int(paginator) + 1):
-                            params['page'] = str(page_num)
-                            tasks.append(params.copy())
-                        del params['page']
+                    for page_num in range(1, int(paginator) + 1):
+                        params['page'] = str(page_num)
+                        tasks.append(params.copy())
+                    del params['page']
 
         print(f"Для обработки найдено {len(tasks)} страниц")
         await asyncio.sleep(2)
@@ -84,30 +121,36 @@ async def get_gather_data():
         for param in alive_it(tasks):
             async with session.get(mos_sud_url, params=param, headers=headers) as page_response:
                 await asyncio.sleep(1)
-                try:
-                    soup = bs(await page_response.text(), 'lxml')
-                    data_rows = soup.find("table", class_="custom_table").find_all('tr')[1:]
-                    state = True if param["publishingState"] == "80c39ae" else False
-                    for row in data_rows:
-                        row = row.find_all('td')
-                        side = row[1].text.strip().split(": ")[-1]
-                        if "Другие участники" in row[1].text.strip():
-                            side = f"{row[1].find('br').previous.text},\n {row[1].find('br').next.next_sibling.text} "
+                # try:
+                soup = bs(await page_response.text(), 'lxml')
+                data_cases = soup.find("table", class_="custom_table").find_all('nobr')[1:]
+                cases_links = [i.find('a').get('href') for i in data_cases]
+                state = True if param["publishingState"] == "80c39ae" else False
+            for case_link in cases_links:
+                case_result = await get_case_data(session, case_link)
+                result.append(case_result)
 
-                        row_result = {"СТ": param["codex"],
-                                      "ДЕЛО": row[0].text.strip(),
-                                      "УЧ": row[3].text.strip().split("№ ")[-1],
-                                      "ТС": "O" if state else "Н",
-                                      "СОСТОЯНИЕ": row[2].text.strip().split(" до ")[-1] if state else
-                                      row[2].text.strip().split(" на ")[-1],
-                                      "СТОРОНЫ": side
-                                      }
 
-                        result.append(row_result)
-                except Exception as e:
-                    with open("error.txt", "a+") as f:
-                        f.write(f'{param["page"]} - {param["codex"]} - {param["publishingState"]} ------> {e}\n')
-                    continue
+
+                        # row = row.find_all('td')
+                        # side = row[1].text.strip().split(": ")[-1]
+                        # if "Другие участники" in row[1].text.strip():
+                        #     side = f"{row[1].find('br').previous.text},\n {row[1].find('br').next.next_sibling.text} "
+                        #
+                        # row_result = {"СТ": param["codex"],
+                        #               "ДЕЛО": row[0].text.strip(),
+                        #               "УЧ": row[3].text.strip().split("№ ")[-1],
+                        #               "ТС": "O" if state else "Н",
+                        #               "СОСТОЯНИЕ": row[2].text.strip().split(" до ")[-1] if state else
+                        #               row[2].text.strip().split(" на ")[-1],
+                        #               "СТОРОНЫ": side
+                        #               }
+                        #
+                        # result.append(row_result)
+                # except Exception as e:
+                #     with open("error.txt", "a+") as f:
+                #         f.write(f'{param["page"]} - {param["codex"]} - {param["publishingState"]} ------> {e}\n')
+                #     continue
 
     wright_to_file(result)
     print("Сбор данных завершён успешно. Вы можете закрыть окно.")
